d1dcabb9e237493d10053ee022482466fafad454
dovid-moshe-crow <70347543+dovid-moshe-crow@users.noreply.github.com>
2020-09-29 14:06:58 +0300

    Initial commit

---
+++ webpack.react.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    mainFields: ["main", "module", "browser"],
+  },
+  entry: "./src/index.tsx",
+  target: "electron-renderer",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000",
+      },
+      {
+        test: /\.(ico|jpe?g|png|gif|webp|svg|mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
+        loader: "file-loader",
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "../dist/renderer"),
+    historyApiFallback: true,
+    compress: true,
+    hot: true,
+    port: 4000,
+    publicPath: "/",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
